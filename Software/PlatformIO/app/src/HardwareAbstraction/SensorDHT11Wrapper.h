5e1e0d8e6eb53998414138a5e41889083f6508bc
mister stam <stam@stam.com>
2024-10-15 17:43:25 +0300

    rename class for better order

--- Software/PlatformIO/app/src/HardwareAbstraction/SensorDHT11Wrapper.h
+++ Software/PlatformIO/app/src/HardwareAbstraction/SensorDHT11Wrapper.h
@@ -1,17 +1,17 @@
-#ifndef WRAPPER_DHT11SENSOR_H
-#define WRAPPER_DHT11SENSOR_H
+#ifndef SENSOR_DHT11_WRAPPER_H
+#define SENSOR_DHT11_WRAPPER_H
 
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-class DHT11Sensor {
+class SensorDHT11Wrapper {
 private:
     DHT dht;  // DHT sensor object
 
 public:
     // Constructor that initializes the DHT object with the sensor pin
-    DHT11Sensor(int sensorPin) : dht(sensorPin, DHT11) {}
+    SensorDHT11Wrapper(int sensorPin) : dht(sensorPin, DHT11) {}
 
     // Initialize the DHT sensor (to be called in setup)
     void init() {
@@ -29,4 +29,4 @@ public:
     }
 };
 
-#endif // WRAPPER_DHT11SENSOR_H
+#endif // SENSOR_DHT11_WRAPPER_H
