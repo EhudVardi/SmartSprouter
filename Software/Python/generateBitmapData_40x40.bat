a2d8fec36cd0ad1b57abbcb63178cdc0cb1bbb19
mister stam <stam@stam.com>
2024-10-17 19:45:16 +0300

    add a python script to generate monochrome bitmap data code, by specifying an image, and target width/heigh add batch files that specify specific width/height, and allow to simply drag an image onto the batch file, which will automatically generate the code for it.  this way there's no need to edit the batch file for different sized for different images.

---
+++ Software/Python/generateBitmapData_40x40.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM Define resolution variables
+set WIDTH=40
+set HEIGHT=40
+
+REM Check if an image file is passed as an argument
+if "%~1"=="" (
+    echo No image file provided. Please drag and drop an image onto this batch file.
+    timeout /t 5
+    exit /b
+)
+
+REM Display the path and name of the dragged file
+echo File passed: %~1
+
+REM Display the current resolution being used
+echo Generating bitmap with resolution %WIDTH%x%HEIGHT%...
+
+REM Specify the full path to your Python script using the batch script's directory
+set SCRIPT_PATH=%~dp0GenerateBitmapFromMonochromeImage.py
+
+REM Run the Python script with the specified width and height
+python "%SCRIPT_PATH%" "%~1" %WIDTH% %HEIGHT%
+
+REM Wait for 5 seconds before closing the console window
+timeout /t 5
