a26c263ee0be40afef25dd6ed40e01ac214a55de
mister stam <stam@stam.com>
2024-10-17 10:28:03 +0300

    encapsulate display properties into a separate header to be shared easily and correctly

--- Software/PlatformIO/app/src/HardwareAbstraction/LcdDisplayHandler.h
+++ Software/PlatformIO/app/src/HardwareAbstraction/LcdDisplayHandler.h
@@ -4,12 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET    -1  // Reset pin not used on ESP32
-#define SCREEN_ADDRESS 0x3C // Change if needed
-
+#include "HardwareAbstraction/LcdDisplayProperties.h"
 
 class LcdDisplayHandler {
 
---
+++ Software/PlatformIO/app/src/HardwareAbstraction/LcdDisplayProperties.h
@@ -0,0 +1,9 @@
+#ifndef LCD_DISPLAY_PROPERTIES_H
+#define LCD_DISPLAY_PROPERTIES_H
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1  // Reset pin not used on ESP32
+#define SCREEN_ADDRESS 0x3C // Change if needed
+
+#endif // LCD_DISPLAY_PROPERTIES_H
