17e8a91fb7b4d2498d0ae11d11c3a6a8c934cf93
mister stam <stam@stam.com>
2024-10-17 23:56:50 +0300

    add application properties header file containing global app props. finish the about page by adding two labels printing the app name and the app version

---
+++ Software/PlatformIO/app/src/ApplicationProperties.h
@@ -0,0 +1,9 @@
+#ifndef APPLICATION_PROPERTIES_H
+#define APPLICATION_PROPERTIES_H
+
+#include <Arduino.h>
+
+const String appNameText = "Smart Sprouter";
+const String appVersionText = "1.0.0";
+
+#endif // APPLICATION_PROPERTIES_H
--- Software/PlatformIO/app/src/GUI/PageAppAbout.h
+++ Software/PlatformIO/app/src/GUI/PageAppAbout.h
@@ -4,19 +4,26 @@
 #include "GUI/PageAppBase.h"
 #include "GUI/Image.h"
 #include "GUI/AppLogoImage.h"
+#include "ApplicationProperties.h"
 
 class PageAppAbout : public PageAppBase {
 protected:
     Image LogoImage;
+    Label appName;
+    Label appVersion;
 
 public:
-    PageAppAbout() : LogoImage( (SCREEN_WIDTH - abstractsproutingseedimage_width) / 2, 
-                                (SCREEN_HEIGHT - abstractsproutingseedimage_height) / 2, 
-                                abstractsproutingseedimage_bitmap, 
-                                abstractsproutingseedimage_width, 
-                                abstractsproutingseedimage_height ) {
+    PageAppAbout() : LogoImage(2, (SCREEN_HEIGHT - abstractsproutingseedimage_height) / 2, 
+                               abstractsproutingseedimage_bitmap, 
+                               abstractsproutingseedimage_width, abstractsproutingseedimage_height),
+                     appName(abstractsproutingseedimage_width + 4, SCREEN_HEIGHT  / 4,
+                             appNameText, FontSize::Small),
+                     appVersion(abstractsproutingseedimage_width + 4, SCREEN_HEIGHT  / 4 + 12,
+                                "Version " + appVersionText, FontSize::Small) {
+        SetTitle("About");
         AddElement(&LogoImage);
-        SetTitle(">About<");
+        AddElement(&appName);
+        AddElement(&appVersion);
     }
 };
 
