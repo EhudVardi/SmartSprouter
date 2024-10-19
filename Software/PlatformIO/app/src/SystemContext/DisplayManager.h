900483e5ada41f55584a5f1393f95822e802a1d8
mister stam <stam@stam.com>
2024-10-19 17:21:51 +0300

    cleanup DisplayManager code from all tests, keeping only the pages instances

--- Software/PlatformIO/app/src/SystemContext/DisplayManager.h
+++ Software/PlatformIO/app/src/SystemContext/DisplayManager.h
@@ -7,16 +7,7 @@
 #include "HardwareAbstraction/LcdDisplayHandler.h"
 #include "SystemContext/DisplayPins.h"
 
-#include "GUI/GuiElement.h"
-#include "GUI/Label.h"
-#include "GUI/CenteredLabel.h"
-#include "GUI/NamedValue.h"
-#include "GUI/TypedNameValue.h"
 #include "GUI/PageBase.h"
-#include "GUI/NamedValueEditState.h"
-#include "GUI/FontSize.h"
-#include "GUI/TypedNameRange.h"
-#include "GUI/NamedRangeEditState.h"
 #include "GUI/PageAppBase.h"
 #include "GUI/PageAppAbout.h"
 #include "GUI/PageAppError.h"
@@ -31,39 +22,15 @@ private:
 
     LcdDisplayHandler* displayHandler = nullptr;
 
-    PageBase myPage;
-    
-    Label myLabel;
-    NamedValueInt intValue;
-    NamedValueFloat floatValue;
-    NamedValuePercent percentValue;
-    CenteredLabel myCenteredLabel;
-    NamedRangePercent myRange;
-    NamedRangeTemperature myRangeTemp;
-
-    PageAppBase myAppPage;
-
     PageAppAbout myAppPageAbout;
-
     PageAppError myAppPageError;
-
     PageAppAbort myAppPageAbort;
-
     PageAppIdle myAppPageIdle;
-
     PageAppRun myAppPageRun;
-
     PageAppSetup myAppPageSetup;
 
 public:
-    DisplayManager() :
-    myLabel(0, 0, "Status", FontSize::Medium), 
-    intValue(0, 16, "Count", FontSize::Small), 
-    floatValue(0, 26, "Temperature", FontSize::Small),  
-    percentValue(0, 36, "Humidity", FontSize::Small), 
-    myCenteredLabel(44, "Centered", FontSize::Medium), 
-    myRange(0, 46, 0, 100, "humid'"),
-    myRangeTemp(0, 56, 0, 100, "temp'") {} // Initialize in the constructor
+    DisplayManager() {}
 
     bool initialize() override;
     void showPage(const std::string& page);
