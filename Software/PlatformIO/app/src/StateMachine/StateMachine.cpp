962fd048a98430d0e1c0548a460fd2d1de92e528
mister stam <stam@stam.com>
2024-10-20 21:05:36 +0300

    finish adding all states instances to the statemachine

--- Software/PlatformIO/app/src/StateMachine/StateMachine.cpp
+++ Software/PlatformIO/app/src/StateMachine/StateMachine.cpp
@@ -1,16 +1,34 @@
 #include "StateMachine/StateMachine.h"
 #include "StateMachine/States/StartingUpState.h"
 #include "StateMachine/States/InitializingState.h"
+#include "StateMachine/States/SystemInErrorState.h"
+#include "StateMachine/States/IdlingState.h"
+#include "StateMachine/States/SettingProcessState.h"
+#include "StateMachine/States/SettingParamState.h"
+#include "StateMachine/States/RunningState.h"
+#include "StateMachine/States/AbortingState.h"
 #include <iostream>
 
 StateMachine::StateMachine() {
     
     auto startingUpState = std::make_shared<StartingUpState>();
     auto initializingState = std::make_shared<InitializingState>();
+    auto systemInErrorState = std::make_shared<SystemInErrorState>();
+    auto idlingState = std::make_shared<IdlingState>();
+    auto settingProcessState = std::make_shared<SettingProcessState>();
+    auto settingParamState = std::make_shared<SettingParamState>();
+    auto runningState = std::make_shared<RunningState>();
+    auto abortingState = std::make_shared<AbortingState>();
 
     // Add states to the state machine
     addState(States::StartingUp, startingUpState);
     addState(States::Initializing, initializingState);
+    addState(States::SystemInError, systemInErrorState);
+    addState(States::Idling, idlingState);
+    addState(States::SettingProcess, settingProcessState);
+    addState(States::SettingParam, settingParamState);
+    addState(States::Running, runningState);
+    addState(States::Aborting, abortingState);
 }
 
 void StateMachine::addState(States stateEnum, std::shared_ptr<State> state) {
