c33d79bd5f640fb6bc666675ecb6d3ce31ff2246
mister stam <stam@stam.com>
2024-10-22 23:10:16 +0300

    add all setup states code and header. using single header and code file for all states

--- Software/PlatformIO/app/src/StateMachine/Setup/StateMachine.cpp
+++ Software/PlatformIO/app/src/StateMachine/Setup/StateMachine.cpp
@@ -1,11 +1,27 @@
 #include "StateMachine/Setup/StateMachine.h"
-// #include "StateMachine/App/States/StartingUpState.h"
+#include "StateMachine/Setup/States/SetupStates.h"
 #include <iostream>
 
 SetupStateMachine::SetupStateMachine() {
-    
-    // auto startingUpState = std::make_shared<StartingUpState>();
 
-    // // Add states to the state machine
-    // addState(AppStates::StartingUp, startingUpState);
+    auto HumidityRangeSelect = std::make_shared<HR_S>();
+    auto HumidityRangeEditMin = std::make_shared<HR_E_MIN>();
+    auto HumidityRangeEditMax = std::make_shared<HR_E_MAX>();
+    auto TemperatureRangeSelect = std::make_shared<TR_S>();
+    auto TemperatureRangeEditMin = std::make_shared<TR_E_MIN>();
+    auto TemperatureRangeEditMax = std::make_shared<TR_E_MAX>();
+    auto DurationSelect = std::make_shared<D_S>();
+    auto DurationEdit = std::make_shared<D_E>();
+    auto Start = std::make_shared<S_S>();
+
+    // Add states to the state machine
+    addState(SetupStates::HumidityRangeSelect, HumidityRangeSelect);
+    addState(SetupStates::HumidityRangeEditMin, HumidityRangeEditMin);
+    addState(SetupStates::HumidityRangeEditMax, HumidityRangeEditMax);
+    addState(SetupStates::TemperatureRangeSelect, TemperatureRangeSelect);
+    addState(SetupStates::TemperatureRangeEditMin, TemperatureRangeEditMin);
+    addState(SetupStates::TemperatureRangeEditMax, TemperatureRangeEditMax);
+    addState(SetupStates::DurationSelect, DurationSelect);
+    addState(SetupStates::DurationEdit, DurationEdit);
+    addState(SetupStates::Start, Start);
 }
---
+++ Software/PlatformIO/app/src/StateMachine/Setup/States/SetupStates.cpp
@@ -0,0 +1,49 @@
+#include "StateMachine/Setup/States/SetupStates.h"
+#include "SystemContext/SystemContext.h"
+#include <iostream>
+
+void HR_S::enter(SystemContext* context) { }
+void HR_S::exit(SystemContext* context) { }
+void HR_S::update(SystemContext* context) { }
+void HR_S::handleInput(SystemContext* context, InputEvent event) { }
+
+void HR_E_MIN::enter(SystemContext* context) { }
+void HR_E_MIN::exit(SystemContext* context) { }
+void HR_E_MIN::update(SystemContext* context) { }
+void HR_E_MIN::handleInput(SystemContext* context, InputEvent event) { }
+
+void HR_E_MAX::enter(SystemContext* context) { }
+void HR_E_MAX::exit(SystemContext* context) { }
+void HR_E_MAX::update(SystemContext* context) { }
+void HR_E_MAX::handleInput(SystemContext* context, InputEvent event) { }
+
+void TR_S::enter(SystemContext* context) { }
+void TR_S::exit(SystemContext* context) { }
+void TR_S::update(SystemContext* context) { }
+void TR_S::handleInput(SystemContext* context, InputEvent event) { }
+
+void TR_E_MIN::enter(SystemContext* context) { }
+void TR_E_MIN::exit(SystemContext* context) { }
+void TR_E_MIN::update(SystemContext* context) { }
+void TR_E_MIN::handleInput(SystemContext* context, InputEvent event) { }
+
+void TR_E_MAX::enter(SystemContext* context) { }
+void TR_E_MAX::exit(SystemContext* context) { }
+void TR_E_MAX::update(SystemContext* context) { }
+void TR_E_MAX::handleInput(SystemContext* context, InputEvent event) { }
+
+void D_S::enter(SystemContext* context) { }
+void D_S::exit(SystemContext* context) { }
+void D_S::update(SystemContext* context) { }
+void D_S::handleInput(SystemContext* context, InputEvent event) { }
+
+void D_E::enter(SystemContext* context) { }
+void D_E::exit(SystemContext* context) { }
+void D_E::update(SystemContext* context) { }
+void D_E::handleInput(SystemContext* context, InputEvent event) { }
+
+void S_S::enter(SystemContext* context) { }
+void S_S::exit(SystemContext* context) { }
+void S_S::update(SystemContext* context) { }
+void S_S::handleInput(SystemContext* context, InputEvent event) { }
+
