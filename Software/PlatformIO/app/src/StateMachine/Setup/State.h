e2ec562f9b0b8b4709c8cafd211c8a96bd168c24
mister stam <stam@stam.com>
2024-10-23 09:36:40 +0300

    add reference to setup page in SetupState base class, and call it from all derived classes

--- Software/PlatformIO/app/src/StateMachine/Setup/State.h
+++ Software/PlatformIO/app/src/StateMachine/Setup/State.h
@@ -7,6 +7,16 @@
 
 class SetupStateMachine; // Forward declare SetupStateMachine
 
-class SetupState : public StateBase<SetupStateMachine> {};
+class SetupState : public StateBase<SetupStateMachine> {
+protected:
+    std::shared_ptr<PageAppSetup> setupPage = nullptr;
+
+public:
+    void enter(SystemContext* context) override {
+        if (!setupPage) {
+            setupPage = context->displayManager->getPageAs<PageAppSetup>(Pages::Setup);
+        }
+    }
+};
 
 #endif // SETUP_STATE_H
