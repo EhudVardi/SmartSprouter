da2ed44efac76b44241ec0c825d63597a2926fe2
mister stam <stam@stam.com>
2024-10-26 11:13:21 +0300

    move TextLabel type into a more general code file - typed label, which will have more typed labels

--- Software/PlatformIO/app/src/GUI/Elements/Simple/CenteredLabel.h
+++ Software/PlatformIO/app/src/GUI/Elements/Simple/CenteredLabel.h
@@ -1,7 +1,7 @@
 #ifndef CENTERED_LABEL_H
 #define CENTERED_LABEL_H
 
-#include "GUI/Elements/Simple/TextLabel.h"
+#include "GUI/Elements/Simple/TypedLabel.h"
 
 class CenteredLabel : public TextLabel {
 private:
