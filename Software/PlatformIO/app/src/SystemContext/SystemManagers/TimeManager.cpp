084f9f5779ffda60d3bf40119f1c8575c1426d1a
mister stam <stam@stam.com>
2024-10-28 11:26:58 +0200

    make the currentTime instance as a pointer so for the runtime of the app, we will have a single DateTime instance that will hold the current time

--- Software/PlatformIO/app/src/SystemContext/SystemManagers/TimeManager.cpp
+++ Software/PlatformIO/app/src/SystemContext/SystemManagers/TimeManager.cpp
@@ -1,7 +1,16 @@
 #include "SystemContext/SystemManagers/TimeManager.h"
 
-bool TimeManager::initialize() {
+// Constructor
+TimeManager::TimeManager() : currentTime(nullptr) {
+    currentTime = new DateTime(); // Initialize currentTime with a new DateTime instance
+}
 
+// Destructor
+TimeManager::~TimeManager() {
+    delete currentTime; // Free the allocated memory
+}
+
+bool TimeManager::initialize() {
     rtcWrapper = new RtcDS3231Wrapper(RTC_SDA_PIN, RTC_SCL_PIN);
     if (!rtcWrapper->begin()) {
         return false; // Initialization failed
@@ -11,21 +20,20 @@ bool TimeManager::initialize() {
 }
 
 void TimeManager::update() {
-    currentTime = rtcWrapper->now(); // Fetch the current time from the RTC
+    *currentTime = rtcWrapper->now(); // Fetch the current time from the RTC and update the value
 }
 
 DateTime& TimeManager::getCurrentTime() {
-    return currentTime;
+    return *currentTime; // Return a reference to the current time instance
 }
 
 void TimeManager::setTimeFromExtSource(time_t& currentEpoch) {
-
-    // adjust UTC time offset
+    // Adjust UTC time offset
     time_t adjustedTime = currentEpoch + utcOffsetHours * 3600;
     // Update the current time instance
-    currentTime = DateTime(adjustedTime);
-    // update hardware RTC with the new time
-    rtcWrapper->adjust(adjustedTime);
+    *currentTime = DateTime(adjustedTime);
+    // Update hardware RTC with the new time
+    rtcWrapper->adjust(*currentTime); // Pass currentTime to adjust
 }
 
 std::string TimeManager::timeToString(time_t time) {
--- Software/PlatformIO/app/src/SystemContext/SystemManagers/TimeManager.h
+++ Software/PlatformIO/app/src/SystemContext/SystemManagers/TimeManager.h
@@ -7,14 +7,17 @@
 
 class TimeManager : public BaseManager {
 private:
-    DateTime currentTime; // Holds the current time
+    DateTime* currentTime; // Pointer to hold the current time
     RtcDS3231Wrapper* rtcWrapper = nullptr; // Instance of RTC wrapper
 
 public:
+    TimeManager(); // Constructor
+    ~TimeManager(); // Destructor
+
     bool initialize() override; // Initialize the RTC and NTP handler
     void update();  // Update the current time using the RTC wrapper
     DateTime& getCurrentTime(); // Get a reference to the current time
-    void setTimeFromExtSource(time_t& currentEpoch); // set time retrieved from external source
+    void setTimeFromExtSource(time_t& currentEpoch); // Set time retrieved from an external source
     std::string timeToString(time_t time); // Function to format time as a human-readable string
 };
 
