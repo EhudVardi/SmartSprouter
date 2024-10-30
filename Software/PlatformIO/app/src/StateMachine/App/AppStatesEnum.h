2b578506b5f51c710b0053088f259a8010b636ad
mister stam <stam@stam.com>
2024-10-30 10:00:45 +0200

    clean non required includes

--- Software/PlatformIO/app/src/StateMachine/App/AppStatesEnum.h
+++ Software/PlatformIO/app/src/StateMachine/App/AppStatesEnum.h
@@ -1,9 +1,6 @@
 #ifndef APP_STATES_ENUMERATION_H
 #define APP_STATES_ENUMERATION_H
 
-#include <unordered_map>
-#include <string>
-
 enum class AppStates {
     StartingUp,
     SystemInError,
--- Software/PlatformIO/app/src/StateMachine/Setup/SetupStatesEnum.h
+++ Software/PlatformIO/app/src/StateMachine/Setup/SetupStatesEnum.h
@@ -1,9 +1,6 @@
 #ifndef SETUP_STATES_ENUMERATION_H
 #define SETUP_STATES_ENUMERATION_H
 
-#include <unordered_map>
-#include <string>
-
 enum class SetupStates {
     HumidityRangeSelect,
     HumidityRangeEditMin,
