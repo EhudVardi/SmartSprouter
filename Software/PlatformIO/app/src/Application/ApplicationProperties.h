98edb67f7e7082557b8f5c70301e7d3a67ea4386
mister stam <stam@stam.com>
2024-11-03 23:05:11 +0200

    Version 1.0.4 - first complete version, before tests

--- Software/PlatformIO/app/src/Application/ApplicationProperties.h
+++ Software/PlatformIO/app/src/Application/ApplicationProperties.h
@@ -4,6 +4,6 @@
 #include <Arduino.h>
 
 const String appNameText = "Smart Sprouter";
-const String appVersionText = "1.0.3";
+const String appVersionText = "1.0.4";
 
 #endif // APPLICATION_PROPERTIES_H
