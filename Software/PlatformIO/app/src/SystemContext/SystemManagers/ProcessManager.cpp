3c8dcdeb7a7bef2fa6db3ce35e43d1f37a17b6e4
mister stam <stam@stam.com>
2024-11-03 20:24:56 +0200

    partial implementation to ProcessManager. manager now:  holds a private pointer to a Process instance and manages it (create, clear, delete, load, configure)  holds a timestamp of the last process update time  holds a PreferenceManager class to provide serialization and flash storage functionality  func createCurrentProcess - creates a new Process instance from given parameters, create process events, their callbacks and add them to the Process  func storeCurrentProcess - attempt to store Proces instance pointer by currentProcess into flash mem  func loadProcessFromStorage - attempt to load existing serialized Process instance from flash mem, and set its events again (these cannot be saved to flash)  func loadProcessFromStorage - delete the Process instance pointed by currentProcess and clear the pointer  func deleteStoredProcess - attempt to delete an existing serialized Process instance in flash mem  func setProcessEventsCallbacks - sets predefined callback functions to a given process instance events using a given actuator manager instance

--- Software/PlatformIO/app/src/SystemContext/SystemManagers/ProcessManager.cpp
+++ Software/PlatformIO/app/src/SystemContext/SystemManagers/ProcessManager.cpp
@@ -2,14 +2,103 @@
 #include "Data/Log.h"
 
 bool ProcessManager::initialize() {
-
+    lastUpdateTime = DateTime(SECONDS_FROM_1970_TO_2000); //set to oldest time, to force first update once started
     return true;
 }
 
-void ProcessManager::saveProcessState() {
-    log("Saving process state to flash...\n");
+void ProcessManager::createCurrentProcess(float minHumidity, float maxHumidity, 
+                              float minTemperature, float maxTemperature, 
+                              DisplayTimeSpan ventsInterval, DisplayTimeSpan ventsDuration, 
+                              DisplayTimeSpan totalDuration, 
+                              TimeManager* timeManager, ActuatorManager* actuatorManager) {
+
+    deleteCurrentProcess();
+    Process* newProcess = new Process(totalDuration, timeManager->getCurrentTime());
+    
+    WindowEvent humidifiersEvent(minHumidity, maxHumidity);
+    WindowEvent airConditionerEvent(minTemperature, maxTemperature);
+    PeriodicEvent ventilatorsEvent(ventsInterval, ventsDuration);
+
+    newProcess->addWindowEvent(WindowEvents::HumidifiersEvent, humidifiersEvent);
+    newProcess->addWindowEvent(WindowEvents::AirConditionersEvent, airConditionerEvent);
+    newProcess->addPeriodicEvent(PeriodicEvents::VentilatorsEvent, ventilatorsEvent);
+
+    setProcessEventsCallbacks(newProcess, actuatorManager);
 }
 
-void ProcessManager::loadProcessState() {
-    log("Loading process state from flash...\n");
+bool ProcessManager::storeCurrentProcess() {
+    return prefHandler->saveObjectToNVS(*currentProcess, currentProcessKey);
+}
+bool ProcessManager::loadProcessFromStorage(ActuatorManager* actuatorManager) {
+    deleteCurrentProcess(); // Clear any existing process to avoid memory leaks
+    Process* loadedProcess = new Process(); // Dynamically allocate a new Process object
+    if (prefHandler->loadObjectFromNVS(*loadedProcess, currentProcessKey)) {
+        currentProcess = loadedProcess;  // Assign the loaded process to currentProcess
+        setProcessEventsCallbacks(currentProcess, actuatorManager); // set events callbacks
+        return true;
+    } else {
+        // Clean up if loading failed
+        delete loadedProcess;
+        return false;
+    }
+}
+void ProcessManager::deleteCurrentProcess() {
+    delete currentProcess;
+    currentProcess = nullptr;
+}
+bool ProcessManager::deleteStoredProcess() {
+    return prefHandler->clearObjectFromNVS(currentProcessKey);
+}
+
+bool ProcessManager::setProcessEventsCallbacks(Process* process, ActuatorManager* actuatorManager){
+    // check if process is not null. attempt to get each event from the process and set its start/stop callbacks
+    if (process == nullptr)
+        return false;
+    
+    WindowEvent* humidifiersEvent = process->getWindowEvent(WindowEvents::HumidifiersEvent);
+    if (humidifiersEvent != nullptr) {
+        humidifiersEvent->setActionCallbacks(
+            [actuatorManager]() { 
+                log("humidifiersEvent Start\n");
+                actuatorManager->SetHumidifiers(HumidifierActions::High);
+            },
+            [actuatorManager]() { 
+                log("humidifiersEvent Stop\n");
+                actuatorManager->SetHumidifiers(HumidifierActions::Off);
+            }
+        );
+    } 
+    else { log("failed to set event callbacks: humidifiersEvent not found in process", LogType::Error); }
+    
+    WindowEvent* airConditionerEvent = process->getWindowEvent(WindowEvents::AirConditionersEvent);
+    if (airConditionerEvent != nullptr) {
+        airConditionerEvent->setActionCallbacks(
+            [actuatorManager]() { 
+                log("airConditionerEvent Start\n");
+                //TODO: start cooler
+            },
+            [actuatorManager]() { 
+                log("airConditionerEvent Stop\n");
+                //TODO: start warmer
+            }
+        );
+    } 
+    else { log("failed to set event callbacks: airConditionerEvent not found in process", LogType::Error); }
+    
+    PeriodicEvent* ventilatorsEvent = process->getPeriodicEvent(PeriodicEvents::VentilatorsEvent);
+    if (ventilatorsEvent != nullptr) {
+        ventilatorsEvent->setActionCallbacks(
+            [actuatorManager]() { 
+                log("ventilators Start\n");
+                //TODO: start ventilators
+            },
+            [actuatorManager]() { 
+                log("ventilators Stop\n");
+                //TODO: start ventilators
+            }
+        );
+    } 
+    else { log("failed to set event callbacks: ventilatorsEvent not found in process", LogType::Error); }
+
+    return true;
 }
--- Software/PlatformIO/app/src/SystemContext/SystemManagers/ProcessManager.h
+++ Software/PlatformIO/app/src/SystemContext/SystemManagers/ProcessManager.h
@@ -2,12 +2,37 @@
 #define PROCESS_MANAGER_H
 
 #include "BaseManager.h"
+#include "Data/PreferenceHandler.h"
+#include "Logic/Process/Process.h"
+#include "SystemContext/SystemManagers/ActuatorManager.h"
+#include "SystemContext/SystemManagers/TimeManager.h"
+#include "Data/Log.h"
 
 class ProcessManager : public BaseManager {
+private:
+    Process* currentProcess = nullptr;
+    DisplayDateTime lastUpdateTime;
+
+    PreferenceHandler* prefHandler;
+    const char* prefNamespaceKey = "AppProcess";
+    const char* currentProcessKey = "CurrProcess";
+
+    bool setProcessEventsCallbacks(Process* process, ActuatorManager* actuatorManager);
+
 public:
+    ProcessManager() { prefHandler = new PreferenceHandler(prefNamespaceKey); }
+    ~ProcessManager() { delete currentProcess; }
+
     bool initialize() override;
-    void saveProcessState();  // Save long-term process state to flash
-    void loadProcessState();  // Load state from flash on boot
+    void createCurrentProcess(float minHumidity, float maxHumidity, 
+                              float minTemperature, float maxTemperature, 
+                              DisplayTimeSpan ventsInterval, DisplayTimeSpan ventsDuration, 
+                              DisplayTimeSpan totalDuration, 
+                              TimeManager* timeManager, ActuatorManager* actuatorManager); //create process from args and set events + their callbacks
+    bool storeCurrentProcess(); //save Process instance in currentProcess into flash memory
+    bool loadProcessFromStorage(ActuatorManager* actuatorManager); //attempt to load stored process into currentProcess, return result status
+    void deleteCurrentProcess(); //clear the local ptr from an instance
+    bool deleteStoredProcess(); //clear a Process instance, stored in flash memory
 };
 
 #endif // PROCESS_MANAGER_H
