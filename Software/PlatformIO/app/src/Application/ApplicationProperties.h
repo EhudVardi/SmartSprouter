41ecb2f80199d3004b55016c410777663883e65f
mister stam <stam@stam.com>
2024-11-08 14:06:16 +0200

    Version 1.0.5

--- Software/PlatformIO/app/src/Application/ApplicationProperties.h
+++ Software/PlatformIO/app/src/Application/ApplicationProperties.h
@@ -4,6 +4,6 @@
 #include <Arduino.h>
 
 const String appNameText = "Smart Sprouter";
-const String appVersionText = "1.0.4";
+const String appVersionText = "1.0.5";
 
 #endif // APPLICATION_PROPERTIES_H
